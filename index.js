f755dff72ef74ebb4edb1f48066a67e2c1dd28f3
Shau Yin Luk <ivan.luk@icloud.com>
2020-05-11 12:03:59 -0500

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,23 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeStr) {
+  let timeS = timeStr.split(":");
+  if (parseInt(timeS[0]) < 12) {
+    return "Good Morning";
+  } else if (parseInt(timeS[0]) < 17)  {
+    return "Good Afternoon"
+  } else if (parseInt(timeS[0]) == 17 && parseInt(timeS[1]) == 0) {
+    return "Good Afternoon"  
+  } else {
+    return "Good Evening"
+  }
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(greeting) {
+  let greet = document.querySelector('#greeting');
+  greet.innerText = `${greeting}`;
+
+}
+
