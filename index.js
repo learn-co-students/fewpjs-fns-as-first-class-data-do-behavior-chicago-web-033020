6821a493f77d35cc882fdc9a13a19e0a43c0ff83
isaacmeedinaa <isaac.meedinaa@gmail.com>
2020-05-11 12:42:21 -0500

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,23 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  let timeStringArr = timeString.split(':');
+  let timeStringArrOne = parseInt(timeStringArr[0]);
+
+  if(timeStringArrOne < 12) {
+    return timeString = 'Good Morning'
+  } else if(timeStringArrOne >= 12 && timeStringArrOne <= 17) {
+    return timeString = 'Good Afternoon'
+  } else if (timeStringArrOne > 17) {
+    return timeString = 'Good Evening'
+  } else {
+    // nothing
+  }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(message) {
+  let content = message;
+  document.getElementById('greeting').innerText = content;
+}
